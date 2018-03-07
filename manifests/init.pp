d1b7f3f9136e831f0f742c61a7137b347901ee6e
root <root@adonyec1.mylabserver.com>
2018-03-07 22:55:42 +0000

    Fixed some bugs

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class ntp (
   String $package_ensure      = $ntp::params::package_ensure,
   String $config_name         = $ntp::params::config_name,
   String $config_file_mode    = $ntp::params::config_file_mode,
-  Array[Strings] $servers     = $ntp::params::servers,
+  Array[String] $servers     = $ntp::params::servers,
   String $service_ensure      = $ntp::params::service_ensure,
   String $service_name        = $ntp::params::service_name,
   Boolean $service_enable     = $ntp::params::service_enable,
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,9 +3,6 @@ class ntp::params {
   $package_ensure = 'present'
   $config_name    = 'ntp.conf'
   $config_file_mode = '0664'
-  $package_ensure = 'present'
-  $config_name    = 'ntp.conf'
-  $config_file_mode = '0664'
   $servers  = ['0.centos.pool.ntp.org','1.centos.pool.ntp.org']
   $service_ensure = 'running'
   $service_enable = true
